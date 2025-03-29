efdab1a7d67e08355d5e6237104a9ef98236d623
alish <alish14.mod@gmail.com>
2025-03-29 07:03:43 +0330

    [implement exit command]

--- app/main.go
+++ app/main.go
@@ -16,6 +16,9 @@ func main() {
 
 		// Wait for user input
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if command == "exit 0\n" {
+			os.Exit(0)
+		}
 
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
