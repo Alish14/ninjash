efb52dd4b25704ff92c8b5f52bae0f38512344c9
alish <alish14.mod@gmail.com>
2025-04-08 23:48:43 +0330

    add cd command and pwd

--- app/main.go
+++ app/main.go
@@ -41,21 +41,32 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
+		homwDir:=os.Getenv("HOME")
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		command = strings.TrimSpace(command)
 		commandSlice:=strings.Split(command," ")
-		if strings.Count(command, "exit")==1 && strings.Index(command, "exit")==0{
+		if commandSlice[0]=="exit"{
 			os.Exit(0)
 		}else if strings.Count(command, "echo")==1 && strings.Index(command, "echo")==0{
 			fmt.Println(strings.TrimSpace(command[4:]))
-		}else if strings.Count(command, "type")>=1 && strings.Index(command, "type")==0{
-			fmt.Println(search(command[4:],builtins))
+		}else if commandSlice[0]=="type"{
+			fmt.Println(search(commandSlice[1],builtins))
 		}else if findExec(commandSlice[0]){
 			out, err := exec.Command(commandSlice[0]).Output()
 			if err != nil {
 				log.Fatal(err)
 			}
 			fmt.Printf("%s", out)
+		}else if commandSlice[0]=="cd"{
+			if len(commandSlice)==1 || commandSlice[1]=="~" {
+				_=os.Chdir(homwDir)
+			}else{
+				err:=os.Chdir(commandSlice[1])
+				if err !=nil{
+					fmt.Printf("cd: %s: No such file or directory\n",commandSlice[1])
+				}
+			}
+
 		}else{
 			fmt.Println(command[:len(command)] + ": command not found")
 		}
